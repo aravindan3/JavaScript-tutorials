704a6d5b485cf7f3b418ac048a15c4a956a51a53
aravindan3 <65463981+aravindan3@users.noreply.github.com>
2020-08-18 00:08:16 +0530

    JavaSCript tutorial practice

---
+++ 01_hello.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+let a = 25;
+let b = 25;
+let c = a+b;
+console.log("total:" +c);
---
+++ 02_datatype.js
@@ -0,0 +1,17 @@
+let a=25;
+let b=true;
+let c="Ram";
+let d=25.56;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+var user={
+    "name":"Ram",
+    "age":25,
+    "isActive":true
+}
+console.log(typeof user);
+console.log(user);
+console.log(user.name);
